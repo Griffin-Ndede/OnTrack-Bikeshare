dd392a8e1469bc06b4d41b6264e3940e99978f40
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-07-07 18:32:27 +0300

    edited README.md

---
+++ README.md
@@ -0,0 +1,51 @@
+# OnTrack Bikeshare platform
+
+## Author
+Griffin Omondi
+
+A description of the bikeshare service.
+# Table of content
+Description
+
+Installation requirement
+
+Technology used
+
+Reference
+
+Licence
+
+Authors info
+
+## Description
+The program is supposed to 
+The index.html file opens up to the main page of the website. On the navigation bar the sign up button has a link that takes you to the registration page where you can sign up or create an account to be able to use the bikeshare service. 
+The user profiles on the navigation bar takes you to a different page that displays the users information.
+
+# Installation requirement
+Either a computer,phone,tablet or an Ipad
+
+An access to the Internet
+
+Web browser preferably Google chrome
+
+# Technologies used
+HTML - to structure the different pages that are present in the program
+
+CSS - to style the different html pages
+
+Javascript - to display the user profile information on the user profile page
+
+
+## References
+Fonts https://fonts.google.com/
+
+Icons https://fontawesome.com/
+
+Server https://user-profiles-jgk3.onrender.com
+
+
+# License
+The project is licensed under the MIT license.
+
+
--- index.js
+++ index.js
@@ -3,12 +3,55 @@ fetch("https://user-profiles-jgk3.onrender.com/users")
 .then(data => {
     console.log(data);
 
+    // display the users list
 const displayUsers = document.getElementById('usernames')
 data.forEach((users) => {
     const displayNames = document.createElement('li')
-    displayNames.textContent = users.email, users.name
+    displayNames.textContent = users.name
 
     displayUsers.appendChild(displayNames);
+
+    // displaying the details of the users
+    displayNames.addEventListener('click', () => {
+    const displayDetails = document.getElementById("usernames")
+    const display1 = document.createElement("div")
+    display1.innerHTML =`
+    <div id = "name">
+        <h1>
+            ${users.name}
+        </h1>
+    </div>
+    <div id = "username">
+        <h1>
+            ${users.username}
+        </h1>
+    </div>
+    <div id = "email">
+        <h1>
+            ${users.email}
+        </h1>
+    </div>
+    <div id = "gender">
+        <h1>
+            ${users.gender}
+        </h1>
+    </div>
+    <div id = "pp">
+        <img src= "${users.gender}" alt = "Profile picture">
+    </div>
+    <div id = "vehicle">
+        <h1>
+            ${users.evehicle}
+        </h1>
+    </div>
+    <div id = "rating">
+        <h1>
+            ${users.rating}
+        </h1>
+    </div>
+    `
+    displayDetails.innerHTML = display1.innerHTML
+        })
     })
 })
 
